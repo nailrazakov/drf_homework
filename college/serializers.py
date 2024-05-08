8f5823b7ceb252e3b9cbba8cf06aa1178f651190
Nail Razakov Win10 <nailrazakov1982@gmail.com>
2024-05-08 08:30:17 +0300

    Serializers task_1

--- college/serializers.py
+++ college/serializers.py
@@ -1,14 +1,20 @@
+from rest_framework.fields import SerializerMethodField
 from rest_framework.serializers import ModelSerializer
 from college.models import Course, Lesson
-
-
-class CourseSerializer(ModelSerializer):
-    class Meta:
-        model = Course
-        fields = '__all__'
 
 
 class LessonSerializer(ModelSerializer):
     class Meta:
         model = Lesson
+        fields = '__all__'
+
+
+class CourseSerializer(ModelSerializer):
+    lessons = SerializerMethodField()
+
+    def get_lessons(self, course):
+        return Lesson.objects.filter(course=course).count()
+
+    class Meta:
+        model = Course
         fields = '__all__'
